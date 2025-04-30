27aaa3095178dd4dc6db7f9ac42c209611ca6364
Geetika Shukla <geetika2903@gmail.com>
2025-04-30 15:36:29 +0530

    Test gdrive for resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 
 
                         <Button className="resume-button" size="small" marginTop="tiny" marginBottom="tiny">
-                            <a href="/geetika-resume.pdf" target="_blank" download>
+                            <a href="https://drive.google.com/uc?export=download&id=1scUVfg3Hs9U8Io-aOYIQw9NYWuKlm1uE" target="_blank" download>
                                 Download Resume
                             </a>
                         </Button>
