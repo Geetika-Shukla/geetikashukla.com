28ecaae73d8f64ea56fa9adb33b9facdf08b373b
Geetika Shukla <geetika2903@gmail.com>
2025-05-05 19:04:14 +0530

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,17 @@ export default function RootLayout({
 
     return (
         <html lang="en">
+            <head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-1XKJ515C1T">
+                </script>
+                <script>
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-1XKJ515C1T');
+                </script>
+            </head>
             <body>
                 <ThemeProvider themeList={listOfThemes} currentTheme="theme-light">
                     <SiteHeader />
